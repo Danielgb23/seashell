51a978a77151fe5c6200d6445f81311d259cf09f
Danielgb23 <danielgb23@gmail.com>
2025-09-09 12:47:28 -0300

    link under cursor into link variable

--- src/seashell.c
+++ src/seashell.c
@@ -1,11 +1,14 @@
 # include <sys/stat.h>
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+
+#define MD4C_USE_UTF8
 # include "md4c.h"
 
-# include <ncurses.h>
-
+# include <curses.h>
+//###################################################################################################
 #define MAX_FILE_SIZE (256 * 1024 * 1024)  // 256 MB
 					   //
 int read_file(const char *filename, char * * out){
@@ -49,11 +52,60 @@ int read_file(const char *filename, char * * out){
 	*out = buffer;
 	return size;
 }
+//###################################################################################################
+//Link stack
+#define MAX_STACK 128
+typedef struct link{
+char * href;
+int xs, ys, xe, ye;
+}Link;
 
-// Md4c 
+char *strndup(const char *s, size_t n) {
+    size_t len = 0;
+    while (s[len] && len < n) len++;   // determine actual length to copy
+
+    char *copy = malloc(len + 1);
+    if (!copy) return NULL;
+
+    for (size_t i = 0; i < len; i++)
+        copy[i] = s[i];
+
+    copy[len] = '\0';
+    return copy;
+}
+
+Link link_arr [MAX_STACK];
+int link_top = -1;
+
+void push_link(const char *href, int size) {
+    	if (link_top < MAX_STACK - 1) {
+        	link_arr[++link_top].href = strndup(href, size);
+    	}
+}
+
+void destroy_link_arr() {
+	for (int i=0; i<link_top; i++){
+         free(link_arr[i].href);
+    }
+}
+
+
+
+void add_link_pos(int xstart, int ystart, int  xend,int  yend){
+link_arr[link_top].xs=xstart;
+link_arr[link_top].ys=ystart;
+link_arr[link_top].xe=xend;
+link_arr[link_top].ye=yend;
+
+
+
+}
+
+//###################################################################################################
+// Md4c + Ncurses
 # define CODE_COLOR 2
 # define TITLE_COLOR 3
-# define LINK_COLOR 3
+# define LINK_COLOR 4
 
 static int current_block = 0;
 static int in_link = 0;
@@ -61,28 +113,43 @@ static int in_em = 0;   // italics
 static int in_strong = 0; // bold
 static int in_under = 0; // under
 static int in_code = 0; // under
+static int save_links = 1;
 
+// parse block
 static int enter_block(MD_BLOCKTYPE type, void *detail, void *userdata) {
     current_block = type;
-    if (type == MD_BLOCK_H) printw("\n");
+    if (type == MD_BLOCK_H){
+	    printw("\n");
+	MD_BLOCK_H_DETAIL *d = (MD_BLOCK_H_DETAIL *)detail;
+	if(d->level ==1) attron( A_BOLD | A_UNDERLINE);
+	if(d->level ==2) attron( A_BOLD );
+    }
     return 0;
 }
-
 static int leave_block(MD_BLOCKTYPE type, void *detail, void *userdata) {
-    if (type == MD_BLOCK_H || type == MD_BLOCK_P) printw("\n");
-    current_block = 0;
-    return 0;
+    	if (type == MD_BLOCK_H || type == MD_BLOCK_P) printw("\n");
+    	current_block = 0;
+	if (type == MD_BLOCK_H){
+		MD_BLOCK_H_DETAIL *d = (MD_BLOCK_H_DETAIL *)detail;
+		if(d->level ==1) attroff( A_BOLD | A_UNDERLINE);
+		if(d->level ==2) attroff( A_BOLD );
+	}
+    	return 0;
 }
-
+//Parse SPAN
 static int enter_span(MD_SPANTYPE type, void *detail, void *userdata) {
-    if (type == MD_SPAN_A) { in_link = 1; }
+    if (type == MD_SPAN_A) {
+	in_link = 1;
+ 	MD_SPAN_A_DETAIL *d = (MD_SPAN_A_DETAIL*) detail;
+	//pushes link into list
+    	push_link(d->href.text, d->href.size);
+    }
     if (type == MD_SPAN_EM) { in_em = 1; attron(A_ITALIC); }
     if (type == MD_SPAN_STRONG) { in_strong = 1; attron( A_BOLD); }
     if (type == MD_SPAN_U) { in_under = 1; attron(A_UNDERLINE); }
     if (type == MD_SPAN_CODE) { in_code = 1; attron(COLOR_PAIR(CODE_COLOR)| A_ITALIC); }
     return 0;
 }
-
 static int leave_span(MD_SPANTYPE type, void *detail, void *userdata) {
     if (type == MD_SPAN_A) { in_link = 0; }
     if (type == MD_SPAN_EM) { in_em = 0; attroff( A_ITALIC);}
@@ -92,20 +159,32 @@ static int leave_span(MD_SPANTYPE type, void *detail, void *userdata) {
     return 0;
 }
 
+//PARSE text
 static int text(MD_TEXTTYPE type, const MD_CHAR *text, MD_SIZE size, void *userdata) {
-    if (current_block == MD_BLOCK_H) {
-	attron( COLOR_PAIR(TITLE_COLOR)|A_BOLD);
-        printw("%.*s", (int)size, text);
-	attroff( COLOR_PAIR(TITLE_COLOR)|A_BOLD);
-    } else if (in_link) {
-	attron( COLOR_PAIR(LINK_COLOR)|A_UNDERLINE);
-        printw( "%.*s", (int)size, text);
-	attroff( COLOR_PAIR(LINK_COLOR)|A_UNDERLINE);
-    } else {
-        printw("%.*s", (int)size, text);
-    }
-    return 0;
+	//titles
+	if (current_block == MD_BLOCK_H) {
+		attron( COLOR_PAIR(TITLE_COLOR) );
+		printw("%.*s", (int)size, text);
+		attroff( COLOR_PAIR(TITLE_COLOR));
+	//LINKs
+	} else if (in_link) {
+		int xs, ys, xe, ye;
+		getyx(stdscr, ys, xs);//get start of link
+		attron( COLOR_PAIR(LINK_COLOR)|A_UNDERLINE);
+	 	printw( "%.*s", (int)size, text);
+		attroff( COLOR_PAIR(LINK_COLOR)|A_UNDERLINE);
+		getyx(stdscr, ye, xe);//get end of link
+		//saves positions of link into list
+		if (save_links)
+			add_link_pos(xs,ys, xe,ye);
+	//Everything else
+	} else {
+		printw("%.*s", (int)size, text);
+	}
+	return 0;
 }
+
+//###################################################################################################
 int main(int argc, const char * argv[]){
 
 	// raw markdown
@@ -114,6 +193,7 @@ int main(int argc, const char * argv[]){
 
 	int ch; //ncurses char
 	int x = 0, y = 0;  // cursor position
+	char * link; //current link under the cursor
 		
 	// End program flag
 	int end= 0 ;
@@ -154,21 +234,25 @@ int main(int argc, const char * argv[]){
 	//COLORS of each enviroment
 	init_pair(1, -1, -1);   // default terminal
 	init_pair(CODE_COLOR, COLOR_GREEN, COLOR_YELLOW) ;
-	init_pair(TITLE_COLOR, COLOR_RED, -1) ;  
-	init_pair(TITLE_COLOR, COLOR_YELLOW, -1)  ;
+	init_pair(TITLE_COLOR, COLOR_MAGENTA, -1) ;  
+	init_pair(LINK_COLOR, COLOR_YELLOW, -1)  ;
 				//
 	attron(COLOR_PAIR(1));
 	//wbkgd(stdscr, COLOR_PAIR(1));
-
+	
+	//render markdown
 	md_parse(raw, size_raw, &parser, NULL);
-
+	save_links = 0;
 	curs_set(1); // cursor appearing
 	move(0, 0);   // cursor to origin
 		      //
     	refresh();
 	while((ch = getch())) {
+	
 		// mode notebook
     		switch(ch) {
+
+			// MOVEMENT OF THE CURSOR
         		case KEY_UP:
 			case 'k':
 				if (y > 0) y--;
@@ -185,7 +269,10 @@ int main(int argc, const char * argv[]){
         		case 'l':
 				if (x < COLS - 1) x++;
 				break;
+
+
         		case '\n':
+				//checks links when enter is pressed
 				break;
 			case 'q':
 				end=1;
@@ -195,15 +282,55 @@ int main(int argc, const char * argv[]){
 				//if (x < COLS - 1) x++;
     		}
 		if (end) break;// ends the main loop
-			       //
-		move(0, 0);   // move hardware cursor
-		md_parse(raw, size_raw, &parser, NULL);
-		move(y, x);   // move hardware cursor
+			       
+		move(0, 0);   // Cursor at 0 to input screen text
+		md_parse(raw, size_raw, &parser, NULL);//render markdown
+						       //
+		// checks for links
+		link = NULL;
+		for(int i=0; i<link_top; i++){
+			//One line link
+			if( link_arr[i].ye == link_arr[i].ys && y == link_arr[i].ys)		
+			{
+				// Cursor inside the link
+				if (x <= link_arr[i].xe-1 && x >= link_arr[i].xs )
+					link=link_arr[i].href;
+			}
+			else{
+				//first line of multiline link
+				if (y==link_arr[i].ys){
+					if(x >= link_arr[i].xs )
+						link=link_arr[i].href;
+					
+				}
+				//last line of multiline link
+				else if (y==link_arr[i].ye){
+					if(x <= link_arr[i].xe-1 )
+						link=link_arr[i].href;
+				}
+				//middle lines of multiline link
+				else if (y>=link_arr[i].ys && y<=link_arr[i].ye){
+						link=link_arr[i].href;
+				}
+				
+			}
+		}
+						       //
+		// Print link at the last line
+    		move(LINES-1, 0);            // move to last line
+    		clrtoeol();                 // clear it
+    		attron(A_REVERSE);          // optional: highlight status bar
+		if(link != NULL)
+    			printw("%s", link);
+    		attroff(A_REVERSE);
+
+		move(y, x);   // move cursor to user position
     		refresh();
 	}
 
     	endwin();               // Cleanup
 				//
+	destroy_link_arr() ;
 
 
 	//printf("%s", raw);
--- test.md
+++ test.md
@@ -6,11 +6,13 @@
 
 *About*
 
-This is a [link](https://example.com) inside Markdown.
+This is a [link](file://./wiki.md) inside Markdown.
 
 This is _underlined_
 
 code: `printf("Hello world!\n")`
 
+
+Unicode ç Ç á à 中文
 
 ### SUB _subtitle_ test
